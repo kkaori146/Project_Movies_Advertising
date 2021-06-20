e44551feeea1d3bd99c78fd36ad536324a819f29
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2021-06-20 14:02:37 +0900

    Inserção dos filmes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 $(function(){
-    $.ajax({
-        url:'https://alunos.b7web.com.br/cinema/',
-        type: 'GET',
-        dataType: 'json',
-        beforeSend: function() {
-          $('.filmes').html('<div class="col-md-12">CArregando...</div>');  
-        },
-        success:function(json) {
-          var html= '';
 
-          for(var i in json) {
-              html += '<div class="col-md-4"><img src="'+json[i].avatar+'" />'+json[i].titulo+'</div>';
-          }
+	$.ajax({
+		url:'https://api.b7web.com.br/cinema/',
+		type:'GET',
+		dataType:'json',
+		beforeSend:function(){
+			$('.filmes').html('<div class="col-md-12">Carregando...</div>');
+		},
+		success:function(json) {
 
-          $('.filmes').html(html);
-        }
-    });
+			var html = '';
+
+			for(var i in json) {
+				html += '<div class="col-md-4"><div class="filme"><img src="'+json[i].avatar+'" />'+json[i].titulo+'</div></div>';
+			}
+
+			$('.filmes').html(html);
+
+		}
+	});
+
 });
